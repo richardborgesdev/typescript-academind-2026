052edba464aa2051cf988dea494600cd2d66de36
richardborgesdev <richardborgesdev@gmail.com>
2026-07-17 17:58:14 -0300

    chore: basics & basics types | literal types

--- projects/2-basics/choices.ts
+++ projects/2-basics/choices.ts
@@ -4,6 +4,11 @@ enum Role {
   Guest,
 }
 
-let userRole: Role = Role.Admin; // 0 -> admin, 1 - guest
+// let userRole: Role = Role.Admin; // 0 -> admin, 1 - guest
+let userRole: 'admin' | 'editor' | 'guest' = 'guest';
 
-userRole = Role.Guest;
+// userRole = Role.Guest;
+
+// ...
+let possibleResults: [-1 | 1, -1 | 1];
+possibleResults = [-1, -1];
