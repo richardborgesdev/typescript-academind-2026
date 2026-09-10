c8e0a888f6696a2e56f1c7dc8f9574a25ddab135
richardborgesdev <richardborgesdev@gmail.com>
2026-09-10 14:03:43 -0300

    chore: classes & interfaces | implementing, use as base type, extending

--- projects/6-interfaces/interfaces.ts
+++ projects/6-interfaces/interfaces.ts
@@ -6,6 +6,30 @@ interface Authenticatable {
   logout(): void;
 }
 
+interface AuthenticatableAdmin extends Authenticatable {
+  role: 'admin' | 'superadmin';
+}
+
+class AuthenticatableUser implements Authenticatable /*, AnotherInterface */ {
+  constructor(public email: string, public password: string) {
+
+  }
+
+  login(email: string): void {
+
+  }
+
+  logout(): void {
+
+  }
+}
+
+// ..
+
+function authenticate(user: Authenticatable) {
+  user.login();
+}
+
 // interface Authenticatable {
 //   role: string,
 // }
